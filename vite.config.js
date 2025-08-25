012269fe05604809855b4a98291e133c3f4a2efd
kwonleo <leo@itdadev.com>
2025-08-25 18:51:51 +0900

    라이브러리 추가 + proxy 세팅 변경

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: env.VITE_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  };
 });
